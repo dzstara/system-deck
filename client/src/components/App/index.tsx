1d18802da47d961c8ca190f133dc8e92037d681b
dzstara <dzstara@pm.me>
2021-02-25 21:54:35 +0100

    List sound sources and volumes

--- client/src/components/App/index.tsx
+++ client/src/components/App/index.tsx
@@ -1,6 +1,7 @@
 import classNames from "classnames";
 import { ObsLoader } from "util/obs";
 import SceneSwitcher from "components/SceneSwitcher";
+import SoundControl from "components/SoundControl";
 import "./style.css";
 
 export default function App() {
@@ -11,6 +12,12 @@ export default function App() {
           <div className="App--item--title">Scenes</div>
 
           <SceneSwitcher />
+        </div>
+
+        <div className={classNames("App--item", "global--scrollbar-style")}>
+          <div className="App--item--title">Volume</div>
+
+          <SoundControl />
         </div>
       </div>
     </ObsLoader>
---
+++ client/src/components/SoundControl/index.tsx
@@ -0,0 +1,121 @@
+import { useEffect, useState } from "react";
+import { obs } from "util/obs";
+import "./style.css";
+
+interface Source {
+  name: string;
+  volume: number;
+  muted: boolean;
+}
+
+export default function SoundControl() {
+  const [sources, setSources] = useState<Array<Source>>([]);
+
+  useEffect(() => {
+    let cancelled = false;
+
+    const SourceVolumeChangedListener = (data: {
+      sourceName: string;
+      volume: number;
+    }) => {
+      setSources((sources) =>
+        sources.map((source) =>
+          source.name === data.sourceName
+            ? {
+                ...source,
+                volume: data.volume,
+              }
+            : source
+        )
+      );
+    };
+
+    const SourceMuteStateChangedListener = (data: {
+      sourceName: string;
+      muted: boolean;
+    }) => {
+      setSources((sources) =>
+        sources.map((source) =>
+          source.name === data.sourceName
+            ? {
+                ...source,
+                muted: data.muted,
+              }
+            : source
+        )
+      );
+    };
+
+    obs.on("SourceVolumeChanged", SourceVolumeChangedListener);
+    obs.on("SourceMuteStateChanged", SourceMuteStateChangedListener);
+
+    getSourceData().then((sources) => {
+      if (!cancelled) setSources(sources);
+    });
+
+    return () => {
+      obs.off("SourceVolumeChanged", SourceVolumeChangedListener);
+      obs.off("SourceMuteStateChanged", SourceMuteStateChangedListener);
+
+      cancelled = true;
+    };
+  }, []);
+
+  return (
+    <div className="SoundControl">
+      {sources.map((source) => (
+        <div key={source.name} className="SoundControl--item">
+          <div className="SoundControl--volume">
+            <div
+              className="SoundControl--volume-bar"
+              style={{ width: source.volume * 100 + "%" }}
+            />
+            <div className="SoundControl--name">
+              <span>{source.name}</span>{" "}
+              <span>{Math.round(source.volume * 100)}%</span>
+            </div>
+          </div>
+
+          <div className="SoundControl--muted">{source.muted ? "❌" : "✔"}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+const audioTypeIds = [
+  "wasapi_output_capture",
+  "wasapi_input_capture",
+  "soundtrack_global_source",
+  "dshow_input",
+];
+
+async function getSourceData() {
+  const response = await obs.send("GetSourcesList");
+
+  return Promise.all(
+    response.sources
+      .filter(
+        (source: any) =>
+          source.type === "input" && audioTypeIds.includes(source.typeId)
+      )
+      .map((source: any) => source.name as string)
+      .map(
+        (source) =>
+          new Promise<Source>((resolve, reject) =>
+            obs
+              .send("GetVolume", { source })
+              .then((response) => {
+                const sourceObject = {
+                  name: source,
+                  volume: response.volume,
+                  muted: response.muted,
+                };
+
+                resolve(sourceObject);
+              })
+              .catch(reject)
+          )
+      )
+  );
+}
